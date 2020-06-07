0ea1ddd1365d1c0ad0d2194aef3bcecab3826e73
XoneK <mr.blackmarch@mail.ru>
2020-06-07 21:36:39 +0300

    load

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,18 @@
             this.Clients = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Employes = new System.Windows.Forms.TabPage();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.button_register = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.показатьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1.SuspendLayout();
             this.Clients.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Employes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -48,7 +55,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(643, 403);
+            this.tabControl1.Size = new System.Drawing.Size(1097, 476);
             this.tabControl1.TabIndex = 1;
             // 
             // Clients
@@ -58,17 +65,20 @@
             this.Clients.Location = new System.Drawing.Point(4, 28);
             this.Clients.Name = "Clients";
             this.Clients.Padding = new System.Windows.Forms.Padding(3);
-            this.Clients.Size = new System.Drawing.Size(635, 371);
+            this.Clients.Size = new System.Drawing.Size(1089, 444);
             this.Clients.TabIndex = 0;
             this.Clients.Text = "Clients";
             this.Clients.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Check});
             this.dataGridView1.Location = new System.Drawing.Point(1, 1);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(624, 368);
+            this.dataGridView1.Size = new System.Drawing.Size(1086, 441);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -76,21 +86,17 @@
             // 
             this.Employes.BackColor = System.Drawing.Color.White;
             this.Employes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Employes.Controls.Add(this.dataGridView2);
             this.Employes.Location = new System.Drawing.Point(4, 28);
             this.Employes.Name = "Employes";
             this.Employes.Padding = new System.Windows.Forms.Padding(3);
-            this.Employes.Size = new System.Drawing.Size(635, 371);
+            this.Employes.Size = new System.Drawing.Size(1089, 444);
             this.Employes.TabIndex = 1;
             this.Employes.Text = "Employes";
             // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
             // button_register
             // 
-            this.button_register.Location = new System.Drawing.Point(680, 360);
+            this.button_register.Location = new System.Drawing.Point(1120, 423);
             this.button_register.Name = "button_register";
             this.button_register.Size = new System.Drawing.Size(108, 39);
             this.button_register.TabIndex = 2;
@@ -98,12 +104,48 @@
             this.button_register.UseVisualStyleBackColor = true;
             this.button_register.Click += new System.EventHandler(this.button_register_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(-6, -2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1093, 444);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Check
+            // 
+            this.Check.Frozen = true;
+            this.Check.HeaderText = "";
+            this.Check.Name = "Check";
+            this.Check.Width = 20;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.показатьВсеToolStripMenuItem,
+            this.изменитьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(146, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // показатьВсеToolStripMenuItem
+            // 
+            this.показатьВсеToolStripMenuItem.Name = "показатьВсеToolStripMenuItem";
+            this.показатьВсеToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.показатьВсеToolStripMenuItem.Text = "Показать все";
+            // 
+            // изменитьToolStripMenuItem
+            // 
+            this.изменитьToolStripMenuItem.Name = "изменитьToolStripMenuItem";
+            this.изменитьToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.изменитьToolStripMenuItem.Text = "Изменить";
+            // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(965, 450);
+            this.ClientSize = new System.Drawing.Size(1266, 488);
             this.Controls.Add(this.button_register);
             this.Controls.Add(this.tabControl1);
             this.Name = "Main_Form";
@@ -111,6 +153,9 @@
             this.tabControl1.ResumeLayout(false);
             this.Clients.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Employes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -120,9 +165,13 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage Clients;
         private System.Windows.Forms.TabPage Employes;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button_register;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Check;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem показатьВсеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изменитьToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,27 +18,42 @@ namespace DataBase
         {
             InitializeComponent();
             DB dB = new DB();
-            string sql = "SELECT * FROM `client`";
+            string sql = "SELECT client.CLIENT_NAME 'Имя', client.BIRTHDATE 'Дата рождения', client.GENDER 'Пол', " +
+                "client.MOBILE_PHONE 'Моб.телефон', client.ADDRESS 'Адрес'," +
+                "employes.EMPLOYEE_NAME 'Контрактер',type_loan.LOAN_NAME 'Вид займа',client.DATA_AGR 'Дата заключения договора',client.WAGES 'Зарплата'" +
+                " FROM `client` INNER JOIN `employes`  ON client.CONTRACTER = employes.EMPLOYEE_ID INNER JOIN `type_loan` ON client.TYPE_LOAN = type_loan.LOAN_ID";
 
-
+            dataGridView1.AllowUserToAddRows = false;
             dB.openConnection();
                 MySqlDataAdapter adapter = new MySqlDataAdapter(sql,dB.getConnection());
                 DataSet ds = new DataSet();
                 adapter.Fill(ds);
-                dataGridView1.DataSource = ds.Tables[1];
+                dataGridView1.DataSource = ds.Tables[0];
             dB.closeConnection();
+            dataGridView1.Columns[1].Width = 250;
+            dataGridView1.Columns[3].Width = 50;
+            dataGridView1.Columns[4].Width = 150;
+            dataGridView1.Columns[5].Width = 250;
+            dataGridView1.Columns[6].Width = 200;
+            dataGridView1.Columns[7].Width = 200;
+            dataGridView1.Columns[7].ContextMenuStrip = contextMenuStrip1;
         }
           
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            MessageBox.Show("HELLO!");
         }
 
         private void button_register_Click(object sender, EventArgs e)
         {
             DB db = new DB();
            
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
         }
     }
 }
